44887254668fd14ed200a28eae1e77c41860acce
rapier456 <127849500+rapier456@users.noreply.github.com>
2023-05-23 16:23:34 +0900

    Add files via upload

---
+++ vuetify1/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Navbar/>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+    name: "App",
+    components : {
+      Navbar
+    },
+    data: () => ({
+    //
+    }),
+};
+
+</script>
